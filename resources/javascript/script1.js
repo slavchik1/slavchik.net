8845a609a3ac80b50ea8113c6e6ab01e7e23172f
slavatamashchuk <slava.tamashchuk@gmail.com>
2023-09-03 22:26:07 +0300

    port text editor to rus and eng, new 404, added 3PO3

---
+++ resources/javascript/script1.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+  var audio = document.getElementById("myAudio");
+  var image = document.getElementById("myImage");
+
+  function playAudio() {
+    audio.play();
+  }
+
+  image.addEventListener("click", playAudio);
+};
