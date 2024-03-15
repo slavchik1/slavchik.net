93d958307337fd8f385dc0adb7b2e64d96750cc5
Slava Tamashchuk <slava.tamashchuk@gmail.com>
2024-03-15 21:55:30 +0200

    Started url_files_crator + add px in some attributes

---
+++ resources/js/url_files_creator.js
@@ -0,0 +1,5 @@
+const selectSystem = document.getElementById("select-system");
+const url = document.getElementById("url");
+const generate = getElementById("generate");
+const downloadA = getElementById("download-a");
+const downloadButton = getElementById("download-button");
