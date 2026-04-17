9ae9dd0592f4599a531047349fddfa8b908a7d2a
Slava Tamashchuk <slava.tamashchuk@gmail.com>
2026-04-17 23:49:11 +0300

    Updaded EML page

--- resources/js/EasyModLoader.js
+++ resources/js/EasyModLoader.js
@@ -1,3 +1,5 @@
 import { marked } from "https://cdn.jsdelivr.net/npm/marked/lib/marked.esm.js";
 
-document.getElementById("area-to-paste").innerHTML = marked.parse(document.getElementById("markdown").textContent);
+const white = `<hr color="white">`;
+
+document.getElementById("area-to-paste").innerHTML = white + marked.parse(document.getElementById("markdown").textContent) + white;
