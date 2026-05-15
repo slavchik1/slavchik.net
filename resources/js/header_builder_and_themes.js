02a88791c858fb9e594bce0209a2f732e9a1b79a
Slava Tamashchuk <slava.tamashchuk@gmail.com>
2026-05-15 22:18:37 +0300

    added hr styles

--- resources/js/header_builder_and_themes.js
+++ resources/js/header_builder_and_themes.js
@@ -6,6 +6,11 @@ function changeTheme(theme) {
     --page-color: #F9F9F9;
     --text-color: black;
     --a-color: blue;
+}
+
+hr {
+    border-top: 1px solid gray;
+    border-bottom: 1px solid #EEEEEE;
 }`,
         "dark": `
 :root {
@@ -13,6 +18,11 @@ function changeTheme(theme) {
     --page-color: #252525;
     --text-color: white;
     --a-color: lightblue;
+}
+
+hr {
+    border-top: 1px solid #171717;
+    border-bottom: 1px solid gray;
 }`
     }
 
